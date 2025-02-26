0d4057a641fdeb0428ea5d6bd73dff1e3a0db00e
Afeayo <afeaayos@gmail.com>
2025-02-26 03:25:37 +0100

    cors

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5500",
   "http://localhost:5500",
+  "https://bamilklens.com.ng/",
+  "https://bamlens-back-end.onrender.com"
 ];
 
 
