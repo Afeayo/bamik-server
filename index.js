6165dc16a97807e6afb966967f6ccdb0e280b806
Afeayo <afeaayos@gmail.com>
2025-02-27 10:45:16 +0100

    public folder

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'BAMS')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Allowed origins for CORS
 const allowedOrigins = [
--- routes/pay.js
+++ routes/pay.js
@@ -188,7 +188,7 @@ router.get('/payment-success', async (req, res) => {
         });
 
         // **Redirect to Success Page**
-        res.redirect(`https://bamilklens.com.ng/success.html`);
+        res.redirect('/success.html');
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error completing registration.' });
